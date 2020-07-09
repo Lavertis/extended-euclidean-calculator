331d1c3ff8ee79a8076f7e4add30424c636bf79b
Lavertis <29688908+Lavertis@users.noreply.github.com>
2020-07-09 19:21:55 +0200

    Remove semicolons from code

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
 function calculate_gcd() {
-    const m = parseInt(document.getElementById("number_1").value, 10);
-    const n = parseInt(document.getElementById("number_2").value, 10);
+    const m = parseInt(document.getElementById("number_1").value, 10)
+    const n = parseInt(document.getElementById("number_2").value, 10)
     if (!input_is_correct(m, n))
-        return;
+        return
 
     let table = ""
     table += open_table()
@@ -13,52 +13,52 @@ function calculate_gcd() {
     const d = result_array[3]
     table += close_table(d, s, t)
 
-    document.getElementById("result_gcd").innerText = "NWD = " + m + " × (" + s + ") + " + n + " × (" + t + ") = " + d;
-    document.getElementById("table").innerHTML = "";
-    document.getElementById("table").innerHTML = table;
+    document.getElementById("result_gcd").innerText = "NWD = " + m + " × (" + s + ") + " + n + " × (" + t + ") = " + d
+    document.getElementById("table").innerHTML = ""
+    document.getElementById("table").innerHTML = table
 }
 
 function input_is_correct(m, n) {
-    let x = 1;
+    let x = 1
     if (m % 1 !== 0 || n % 1 !== 0 || m === Infinity || m === -Infinity || n === Infinity || n === -Infinity) {
         document.getElementById("result_gcd").innerText = "Niepoprawne dane"
-        x = 0;
+        x = 0
     } else if (m < 1 || n < 1) {
-        document.getElementById("result_gcd").innerText = "Liczby muszą być większe od 0";
-        x = 0;
+        document.getElementById("result_gcd").innerText = "Liczby muszą być większe od 0"
+        x = 0
     }
-    document.getElementById("table").innerHTML = "";
+    document.getElementById("table").innerHTML = ""
     return Boolean(x)
 }
 
 function fill_table(m, n) {
-    let table = "";
-    let d = m;
-    let d_prime = n;
-    let d_prime_temp;
-    let s = 1;
-    let s_prime = 0;
-    let s_prime_temp;
-    let t = 0;
-    let t_prime = 1;
-    let t_prime_temp;
-    let q;
+    let table = ""
+    let d = m
+    let d_prime = n
+    let d_prime_temp
+    let s = 1
+    let s_prime = 0
+    let s_prime_temp
+    let t = 0
+    let t_prime = 1
+    let t_prime_temp
+    let q
 
     while (d_prime !== 0) {
-        table += "<tr><td>" + d + "</td><td>" + d_prime + "</td>";
-        q = Math.floor(d / d_prime);
-        d_prime_temp = d_prime;
-        d_prime = d - q * d_prime;
-        d = d_prime_temp;
-        table += "<td>" + s + "</td><td>" + s_prime + "</td>";
-        s_prime_temp = s_prime;
-        s_prime = s - s_prime * q;
-        s = s_prime_temp;
-        table += "<td>" + t + "</td><td>" + t_prime + "</td>";
-        t_prime_temp = t_prime;
-        t_prime = t - t_prime * q;
-        t = t_prime_temp;
-        table += "<td>" + q + "</td></tr>";
+        table += "<tr><td>" + d + "</td><td>" + d_prime + "</td>"
+        q = Math.floor(d / d_prime)
+        d_prime_temp = d_prime
+        d_prime = d - q * d_prime
+        d = d_prime_temp
+        table += "<td>" + s + "</td><td>" + s_prime + "</td>"
+        s_prime_temp = s_prime
+        s_prime = s - s_prime * q
+        s = s_prime_temp
+        table += "<td>" + t + "</td><td>" + t_prime + "</td>"
+        t_prime_temp = t_prime
+        t_prime = t - t_prime * q
+        t = t_prime_temp
+        table += "<td>" + q + "</td></tr>"
     }
     return [table, s, t, d]
 }
@@ -66,10 +66,10 @@ function fill_table(m, n) {
 function open_table() {
     return "<table><tr><th><span class='bold'>d</span></th><th><span class='bold'>d'</span></th><th>" +
         "<span class='bold'>s</span></th><th><span class='bold'>s'</span>" + "</th><th><span class='bold'>t</span>" +
-        "</th><th><span class='bold'>t'</span></th><th><span class='bold'>q</span></th></tr>";
+        "</th><th><span class='bold'>t'</span></th><th><span class='bold'>q</span></th></tr>"
 }
 
 function close_table(d, s, t) {
     return "<tr><td>" + d + "</td>" + "<td>" + 0 + "</td>" + "<td>" + s + "</td>" + "<td></td>" + "<td>" + t +
-        "</td><td></td><td></td></tr></table>";
+        "</td><td></td><td></td></tr></table>"
 }
