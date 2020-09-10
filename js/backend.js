aa6da789f4d69972cbec216780b0088d35683870
Lavertis <29688908+Lavertis@users.noreply.github.com>
2020-09-10 19:24:50 +0200

    Docs(js) - add JSDoc documentation

--- js/backend.js
+++ js/backend.js
@@ -1,12 +1,14 @@
-function getGCDTable(m, n) {
-    return calculateTable(m, n)
-}
-
-function calculateTable(m, n) {
+/**
+ * Calculates GCD table.
+ * @param  {Number} num1 The first number
+ * @param  {Number} num2 The second number
+ * @return {Array}       Calculated GCD array of numbers
+ */
+function getGCDTable(num1, num2) {
     let tableArray = []
     let temp
-    let d = m
-    let d_prime = n
+    let d = num1
+    let d_prime = num2
     let s = 1
     let s_prime = 0
     let t = 0
@@ -35,6 +37,10 @@ function calculateTable(m, n) {
     return tableArray
 }
 
+/**
+ * Creates a new TableRow.
+ * @class
+ */
 class TableRow {
     constructor(d, d_prime, s, s_prime, t, t_prime, q) {
         this.d = d
@@ -47,7 +53,18 @@ class TableRow {
     }
 }
 
-function addCurrentRowToArray(tableArray, d, d_prime, s, s_prime, t, t_prime, q) {
+/**
+ * Adds new TableRow object to the array.
+ * @param {Array}   array   GCD array
+ * @param {Number}  d
+ * @param {Number}  d_prime
+ * @param {Number}  s
+ * @param {Number}  s_prime
+ * @param {Number}  t
+ * @param {Number}  t_prime
+ * @param {Number}  q
+ */
+function addCurrentRowToArray(array, d, d_prime, s, s_prime, t, t_prime, q) {
     const currentRow = new TableRow(d, d_prime, s, s_prime, t, t_prime, q)
-    tableArray.push(currentRow)
+    array.push(currentRow)
 }
--- js/frontend.js
+++ js/frontend.js
@@ -1,3 +1,6 @@
+/**
+ * Sends a request for GCD calculation after checking for valid input.
+ */
 function buttonClick() {
     document.getElementById("table").innerHTML = ""
     const m = parseInt(document.getElementById("number-1").value, 10)
@@ -11,6 +14,10 @@ function buttonClick() {
         showResultOnPage(m, n)
 }
 
+/**
+ * Creates a new InputValidator. Validates the input at the moment of object creation.
+ * @class
+ */
 class InputValidator {
     #m = 0
     #n = 0
@@ -23,43 +30,73 @@ class InputValidator {
         this.#validate()
     }
 
+    // By default the input is invalid, which can be only changed at the moment of object creation by this function
     #validate() {
-        if (!(Number.isInteger(this.#m) && Number.isInteger(this.#n)))
+        if (!(Number.isInteger(this.#m) && Number.isInteger(this.#n))) // Check if both numbers are integers
             this.errorMsg = "Incorrect data"
-        else if (this.#m < 1 || this.#n < 1)
+        else if (this.#m < 1 || this.#n < 1) // Check if numbers are greater than 0
             this.errorMsg = "Numbers must be greater than 0"
-        else
+        else // Mark the input as valid
             this.valid = true
     }
 }
 
+/**
+ * Displays error message.
+ * @param {String} msg Error message
+ */
 function showErrorMessage(msg) {
     document.getElementById("equation").innerText = msg
 }
 
-function showResultOnPage(m, n) {
-    const resultArray = getGCDTable(m, n)
+/**
+ * Shows whole result on page.
+ * @param {Number} num1 The first number
+ * @param {Number} num2 The second number
+ */
+function showResultOnPage(num1, num2) {
+    const resultArray = getGCDTable(num1, num2)
     const final_s = resultArray[resultArray.length - 1].s
     const final_t = resultArray[resultArray.length - 1].t
     const final_d = resultArray[resultArray.length - 1].d
 
-    displayEquation(m, n, final_s, final_t, final_d)
+    displayEquation(num1, num2, final_s, final_t, final_d)
     displayTable(resultArray, final_d, final_s, final_t)
 }
 
-function displayEquation(m, n, s, t, d) {
+/**
+ * Displays equation.
+ * @param {Number}  num1 The first number
+ * @param {Number}  num2 The second number
+ * @param {Number}  s    First multiplier
+ * @param {Number}  t    Second multiplier
+ * @param {Number}  d    GCD result
+ */
+function displayEquation(num1, num2, s, t, d) {
     document.getElementById("equation").innerHTML =
-        "GCD(" + m + ", " + n + ") = " + m + " × <strong>" + s + "</strong> + " + n + " × <strong>" + t + "</strong> = " + d
+        "GCD(" + num1 + ", " + num2 + ") = " + num1 + " × <strong>" + s + "</strong> + " + num2 + " × <strong>" + t + "</strong> = " + d
 }
 
-function displayTable(resultArray, d, s, t) {
+/**
+ * Displays the GCD table.
+ * @param {Array}   tableArray Array of TableRow objects
+ * @param {Number}  d GCD result
+ * @param {Number}  s First multiplier
+ * @param {Number}  t Second multiplier
+ */
+function displayTable(tableArray, d, s, t) {
     let table = ""
     table += openTable()
-    table += fillTable(resultArray)
+    table += fillTable(tableArray)
     table += closeTable(d, s, t)
     document.getElementById("table").innerHTML = table
 }
 
+/**
+ * Creates rows of the table.
+ * @param   {Array} tableArray Array of TableRow objects
+ * @return  {String}           Rows of the GCD table with html tr, td tags
+ */
 function fillTable(tableArray) {
     let table = ""
     for (let i = 0; i < tableArray.length - 1; i++) {
@@ -71,12 +108,23 @@ function fillTable(tableArray) {
     return table
 }
 
+/**
+ * Creates table header.
+ * @return {string} Table header with html th tags
+ */
 function openTable() {
     return "<table><tr><th><strong>d</strong></th><th><strong>d'</strong></th><th>" +
         "<strong>s</strong></th><th><strong>s'</strong>" + "</th><th><strong>t</strong>" +
         "</th><th><strong>t'</strong></th><th><strong>q</strong></th></tr>"
 }
 
+/**
+ * Creates last table row.
+ * @param {Number}  d GCD result
+ * @param {Number}  s First multiplier
+ * @param {Number}  t Second multiplier
+ * @return {string}   Last table row with table closing html tags
+ */
 function closeTable(d, s, t) {
     return "<tr><td><strong>" + d + "</strong></td>" + "<td>" + 0 + "</td>" + "<td><strong>" + s +
         "</strong></td>" + "<td></td>" + "<td><strong>" + t + "</strong></td><td></td><td></td></tr></table>"
